3dc76d93603cc338d59db65db8031463630b880e
Juan Miguel Manalo <jm.cruz.manalo@gmail.com>
2018-07-28 10:46:03 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({
+    hi: 'there'
+  });
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Started listening to port ${PORT}`);
+});
